3e4d6e978de02898d6566cb6a598d55986b9a7af
Justin Green <justingreen@Justins-MacBook-Air.local>
2023-11-27 10:45:19 -0500

    save

--- src/components/Navbar/MobileMenu.jsx
+++ src/components/Navbar/MobileMenu.jsx
@@ -5,17 +5,17 @@ const MobileMenu = ({showMobile}) => {
   return (
     <section className={showMobile ? "mobile-menu" : "mobile-menu hide-mobile-menu"}>
         <ul className="mobile-links">
-            <li className="mobile-link-item">
+            <li onClick={()=>setShowMobile(false)} className="mobile-link-item">
                 <Link className="mobile-link" to="/">
                    <h5> Home </h5>
                 </Link>
             </li>
-            <li className="mobile-link-item">
+            <li onClick={()=>setShowMobile(false)} className="mobile-link-item">
                 <Link className="mobile-link" to="/portfolio">
                 <h5> Portfolio </h5>
                 </Link>
             </li>
-            <li className="mobile-link-item">
+            <li onClick={()=>setShowMobile(false)} className="mobile-link-item">
                 <Link className="mobile-link" to="/contact">
                     <h5>Contact Me </h5>
                 </Link>
